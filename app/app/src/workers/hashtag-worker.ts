264bd0a6e2035e6bdc212537c318b87299469b70
Kevin <kevin.kds80@gmail.com>
2024-11-10 13:38:40 -0300

    set worker to save and detect anomalies

--- app/app/src/workers/hashtag-worker.ts
+++ app/app/src/workers/hashtag-worker.ts
@@ -3,24 +3,36 @@ import { Worker, Job } from 'bullmq'
 import picker from '../clients/client-picker'
 import filters from '../filters/filter-picker'
 import { isAnomally } from 'src/filters/filters'
+import { BatchModel } from 'src/schema/batch';
+import { RegisterModel } from 'src/schema/register';
 
 const logger = new Logger('Hashtag Worker')
 
 const worker = new Worker('hashtag', async (job: Job) => {
-  console.log('JOB:: ', job.data)
+  logger.debug(job.data)
   try {
     const mapper = picker[job.data.source]
     logger.log(`Getting data from ${job.data.source} client`)
     await mapper.getAndSetData(job.data)
-    /* const { batch, registers } =  */mapper.mapData()
-    //const filter = filters[job.data.filter]
+    const { batch, registers } = mapper.mapData()
+    const filter = filters[job.data.filter]
     logger.log('Detecting anomaly')
 
+    const batches = await BatchModel.find({ 
+      hashtag: job.data.hashtag, 
+      source: job.data.source  
+    }).limit(100).exec()
+
+    const expected = filter(batches)
+    if (expected > 0 && isAnomally(expected, batch.total)) {
+      logger.warn(`Anomaly detected for batch id: ${batch.batchId}`)
+    }
+
     logger.log('Saving batch')
+    await new BatchModel(batch).save()
 
     logger.log('Saving registers')
-    
-    //console.log(batch, registers)
+    await RegisterModel.collection.insertMany(registers)
   } catch (error) {
     logger.error(error.message)
   }
